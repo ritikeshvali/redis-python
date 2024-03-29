46e3d7b77ba62a3545e8a99ddaf84f8c98deb021
Ritikesh Vali <ritikeshvali2001@gmail.com>
2024-03-30 05:19:43 +0530

    Respond to ping

--- app/main.py
+++ app/main.py
@@ -1,11 +1,24 @@
 import socket
+import re
+
+def handle_connection(conn, addr):
+    data: bytes = conn.recv(1024).decode()
+    print(data)
+    pattern = re.compile(r'.*ping.*')
+    matches = re.match(pattern, data)
+    if matches:
+        response = f"+PONG\r\n"
+        conn.send(response.encode())
+    conn.close()
 
 def main():
-
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept()
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Received a connection from {client_address}")
+        handle_connection(client_socket, client_address)
 
 
 if __name__ == "__main__":
